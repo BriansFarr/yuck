d6da662e054c20fa8ba47aecddb2580ecd2fd597
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2021-02-15 18:34:55 -0500

    landing page edit

--- yuck-hub/src/Components/Landing.jsx
+++ yuck-hub/src/Components/Landing.jsx
@@ -8,7 +8,7 @@ export default function Landing() {
     <div className="Home">
       <h1>Welcome to YuckHub!</h1>
       <h2>Click on your city and then search by restaurant name.</h2>
-      <h2>Most Recent Ratings:</h2>
+      <h2>Most Recent NYC Ratings:</h2>
       <LandingPageSearch />
     </div>
   )
