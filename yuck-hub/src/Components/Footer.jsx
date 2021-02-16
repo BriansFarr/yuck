707ac4992e1ccf673ebfbeda0479aa694c1ead81
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2021-02-15 22:23:44 -0500

    update footer

--- yuck-hub/src/Components/Footer.jsx
+++ yuck-hub/src/Components/Footer.jsx
@@ -4,7 +4,10 @@ import '../CSS/Footer.css'
 export default function Footer() {
   return (
     <div className='footer'>
-      <h1>Footer</h1>
+      <h1>Contact me:</h1>
+      <form action="https://brian-farr.com">
+    <input type="submit" value="Brian Farr" />
+      </form>
     </div>
   )
 }
